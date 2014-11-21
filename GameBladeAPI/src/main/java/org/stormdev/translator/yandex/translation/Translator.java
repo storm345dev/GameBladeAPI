5079b799200ad264690f195e9fb06d69ff3aa135
storm345 <storm345dev@gmail.com>
2014-11-21 20:50:27 +0000

    Added custom written Yandex Translation API

---
+++ GameBladeAPI/src/main/java/org/stormdev/translator/yandex/translation/Translator.java
@@ -0,0 +1,23 @@
+package org.stormdev.translator.yandex.translation;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.stormdev.translator.yandex.errors.YandexException;
+import org.stormdev.translator.yandex.errors.YandexUnsupportedLanguageException;
+import org.stormdev.translator.yandex.languages.Lang;
+
+public class Translator {
+	private static YandexConnection con;
+	static {
+		con = new YandexConnection("trnsl.1.1.20141121T183745Z.640aedb93bcd04b7.1ac5bb0cabfa32bb8ffffb3fdb9eacd147685efb");
+	}
+	
+	public Lang detectLanguage(String text) throws YandexUnsupportedLanguageException, YandexException, MalformedURLException, IOException{
+		return con.getLang(text);
+	}
+	
+	public String translate(Lang from, Lang to, String text) throws YandexException, MalformedURLException, IOException{
+		return con.translate(from, to, text);
+	}
+}

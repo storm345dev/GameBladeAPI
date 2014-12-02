fcc26faceef0c1d24873790eb509803806175603
storm345 <storm345dev@gmail.com>
2014-12-02 17:51:40 +0000

    Fix the subtitle bar (Take non-json strings)

--- GameBladeAPI/src/main/java/org/stormdev/gbapi/storm/misc/SubtitleBar.java
+++ GameBladeAPI/src/main/java/org/stormdev/gbapi/storm/misc/SubtitleBar.java
@@ -2,11 +2,44 @@ package org.stormdev.gbapi.storm.misc;
 
 import java.lang.reflect.Method;
 
+import org.bukkit.craftbukkit.libs.com.google.gson.Gson;
 import org.bukkit.entity.Player;
 
 
 public class SubtitleBar {
 	public static void sendSubtitle(Player player, String message){ //Sends to their chat if 1.7, not 1.8
+		try {
+			Class<?> IChatBaseComponent = Reflect.getNMSClass("IChatBaseComponent");
+			Class<?> PacketPlayOutChat = Reflect.getNMSClass("PacketPlayOutChat");
+			Class<?> ChatSerializer = Reflect.getNMSClass("ChatSerializer");
+			Method aChatSerializer = ChatSerializer.getDeclaredMethod("a", String.class);
+			
+			/*Class<?> c = PacketPlayOutChat.class;
+			for(Constructor<?> con:c.getDeclaredConstructors()){
+				StringBuilder sb = new StringBuilder();
+				for(Class<?> param:con.getParameterTypes()){
+					sb.append("[").append(param.getName()).append("]");
+				}
+				System.out.println(sb.toString());
+			}*/
+			
+			/*JsonParser jp = new JsonParser();
+			JsonReader jr = new JsonReader(new StringReader(message));
+			jr.setLenient(true);
+			message = jp.parse(jr).getAsString();*/
+			
+			message = new Gson().toJson(message);
+			
+			Object o = aChatSerializer.invoke(null, message);
+			Object packet = PacketPlayOutChat.getConstructor(IChatBaseComponent, byte.class).newInstance(IChatBaseComponent.cast(o), (byte)2);
+			Reflect.sendPacket(player, packet);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void sendJSONSubtitle(Player player, String message){ //Sends to their chat if 1.7, not 1.8
 		try {
 			Class<?> IChatBaseComponent = Reflect.getNMSClass("IChatBaseComponent");
 			Class<?> PacketPlayOutChat = Reflect.getNMSClass("PacketPlayOutChat");
